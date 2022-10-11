f031c9f4b288aaa7458fb0d6ed6db8370ae9889f
azimabdurasulov <azimjon20050604@gmail.com>
2022-10-11 11:43:33 +0500

    if05.py

--- if05.py
+++ if05.py
@@ -22,24 +22,19 @@ def main(n):
     n //= 10
 
     max = x1 
-    index = 1
 
     if max < x2:
         max = x2
-        index += 1
 
     if max < x3:
         max = x3
-        index += 1
 
     if max < x4:
         max = x4
-        index += 1
 
     if max < x5:
         max = x5 
-        index += 1
     
-    return max, index
+    return max
 
 print(main(89874))
