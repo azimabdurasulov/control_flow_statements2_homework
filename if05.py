cca94e6010869b3bf31c10cf8b0b4a965acb0c17
azimabdurasulov <azimjon20050604@gmail.com>
2022-10-11 11:40:37 +0500

    if05.py

--- if05.py
+++ if05.py
@@ -6,4 +6,40 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    x1 = n % 10
+    n //= 10 
+
+    x2 = n % 10
+    n //= 10
+
+    x3 = n % 10
+    n //= 10
+
+    x4 = n % 10
+    n //= 10
+
+    x5 = n % 10
+    n //= 10
+
+    max = x1 
+    index = 1
+
+    if max < x2:
+        max = x2
+        index += 1
+
+    if max < x3:
+        max = x3
+        index += 1
+
+    if max < x4:
+        max = x4
+        index += 1
+
+    if max < x5:
+        max = x5 
+        index += 1
+    
+    return max, index
+
+print(main(89874))
